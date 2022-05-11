86915d8ac0f7c00fd777713479ae1518261ba0f0
raul3222 <sh_raul08@mail.ru>
2022-05-11 21:37:19 +0200

    Initial Commit

---
+++ FirstMapKitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstMapKitApp
+//
+//  Created by Raul Shafigin on 11.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
