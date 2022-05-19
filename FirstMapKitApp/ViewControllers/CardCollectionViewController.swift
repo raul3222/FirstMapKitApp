9f82ee6ceb997faecf01758e4790345d531427f7
raul3222 <sh_raul08@mail.ru>
2022-05-19 23:24:28 +0200

    sideMenu, backgroundImage added

--- FirstMapKitApp/ViewControllers/CardCollectionViewController.swift
+++ FirstMapKitApp/ViewControllers/CardCollectionViewController.swift
@@ -9,7 +9,11 @@ import UIKit
 
 
 class CardCollectionViewController: UIViewController {
+    @IBOutlet weak var newView: UIView!
+    @IBOutlet weak var backgroundImage: UIImageView!
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var tableView: UITableView!
+    var shouldMove = false
     var isReceived = false
     var sights: [Sight]! {
         didSet {
@@ -21,11 +25,65 @@ class CardCollectionViewController: UIViewController {
     }
         override func viewDidLoad() {
             super.viewDidLoad()
+            tableView.dataSource = self
             self.overrideUserInterfaceStyle = .light //игнорируем темную тему Iphone
             guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return }
             layout.scrollDirection = .horizontal
+            let swipeRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(self.handleSwipe))
+            swipeRecognizer.direction = .left
+            view.addGestureRecognizer(swipeRecognizer)
+            backgroundImage.image = UIImage(named: "kaliningrad")
+            backgroundImage.alpha = 0.3
+            
+        }
+    
+    @objc private func handleSwipe(sender: UISwipeGestureRecognizer) {
+        if shouldMove {
+            shouldMove = false
+            UIView.animate(withDuration: 0.5,
+                           delay: 0,
+                           usingSpringWithDamping: 0.8,
+                           initialSpringVelocity: 0,
+                           options: .curveEaseInOut,
+                           animations: {
+                            self.newView.frame.origin.x = 0
+            }) { (finished) in
+                
+            }
+
         }
     }
+    @IBAction func showMenuButton(_ sender: Any) {
+        if !shouldMove {
+            print("false")
+            // показываем menu
+            shouldMove = true
+            UIView.animate(withDuration: 0.5,
+                           delay: 0,
+                           usingSpringWithDamping: 0.8,
+                           initialSpringVelocity: 0,
+                           options: .curveEaseInOut,
+                           animations: {
+                            self.newView.frame.origin.x = self.newView.frame.width - 200
+            }) { (finished) in
+                
+            }
+        } else {
+            // убираем menu
+            shouldMove = false
+            UIView.animate(withDuration: 0.5,
+                           delay: 0,
+                           usingSpringWithDamping: 0.8,
+                           initialSpringVelocity: 0,
+                           options: .curveEaseInOut,
+                           animations: {
+                            self.newView.frame.origin.x = 0
+            }) { (finished) in
+                
+            }
+        }
+    }
+}
 
 extension CardCollectionViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
      func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -52,5 +110,24 @@ extension CardCollectionViewController: UICollectionViewDataSource, UICollection
         let sight = sights[indexPath.first!.row]
             detailedVC.sight = sight
     }
+    
+}
+
+extension CardCollectionViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "menuCell", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = "\(indexPath.row) item"
+        cell.contentConfiguration = content
+        return cell
+    }
+    
     
 }
